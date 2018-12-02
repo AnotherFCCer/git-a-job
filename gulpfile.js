f16062659eb3e9f8b3fe9bd55347a51b3aa30ac4
David <david_chou@ymail.com>
2018-12-02 18:00:37 -0500

    progress so far through lesson 12

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+const gulp = require("gulp"),
+
+      watch = require("gulp-watch"),
+      postcss = require("gulp-postcss"),
+      autoprefixer = require("autoprefixer"),
+      cssvars = require("postcss-simple-vars"),
+      nested = require("postcss-nested")
+
+gulp.task("default", function() {
+
+  console.log("Hooray -- you created a Gulp task (and in Git Bash's VI CLI text-editor, too!)!")
+})
+
+gulp.task("something_else", function() {
+
+  console.log("Hooray -- another Gulp task created (also in VI as well!)!")
+
+})
+
+gulp.task("html", function() {
+
+  console.log("Imagine something useful being done to the HTML here!")
+
+})
+
+gulp.task("css", function() {
+  return gulp.src("./app/assets/styles/styles.css")
+  .pipe(postcss([cssvars, nested, autoprefixer]))
+  .pipe(gulp.dest("./app/temp/styles"))
+})
+
+gulp.task("watch", function() {
+
+  watch("./app/index.html", function() {
+    gulp.start("html")
+  })
+
+  watch("./app/assets/styles/**/*.css", function() {
+    gulp.start("css")
+  })
+
+})
